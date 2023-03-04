43ae7c2988363e5a870357a3c0efdd3812981d7c
Enzo <53284465+Enzo-MiMan@users.noreply.github.com>
2023-03-04 16:23:28 +0800

    Create model_MobileNet_v2.py

---
+++ classification/MobileNet/model_MobileNet_v2.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+def _make_divisible(ch, divisor=8, min_ch=None):
+    """用于保证所有的 channel 都能被8整除"""
+    if min_ch is None:
+        min_ch = divisor
+    # 四舍五入
+    new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_ch < 0.9 * ch:
+        new_ch += divisor
+    return new_ch
+
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU6(inplace=True)
+        )
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, in_channel, out_channel, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        hidden_channel = in_channel * expand_ratio
+        self.use_shortcut = stride == 1 and in_channel == out_channel
+        # there is no relu after narrow the channel by conv1x1
+        self.conv = nn.Sequential(
+            ConvBNReLU(in_channel, hidden_channel, kernel_size=1),
+            ConvBNReLU(hidden_channel, hidden_channel, stride=stride, groups=hidden_channel),
+            nn.Conv2d(hidden_channel, out_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channel),
+        )
+
+    def forward(self, x):
+        if self.use_shortcut:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
+        super(MobileNetV2, self).__init__()
+        input_channel = _make_divisible(32 * alpha, round_nearest)
+        last_channel = _make_divisible(1280 * alpha, round_nearest)
+
+        inverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        features = [ConvBNReLU(3, input_channel, stride=2)]
+
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = _make_divisible(c * alpha, round_nearest)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(InvertedResidual(input_channel, output_channel, stride, expand_ratio=t))
+                input_channel = output_channel
+
+        features.append(ConvBNReLU(input_channel, last_channel, 1))
+        self.features = nn.Sequential(*features)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(last_channel, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+# net = MobileNetV2()
+# print(summary(net, (3, 224, 224)))
+
